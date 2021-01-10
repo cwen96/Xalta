9879bcd2ca797511bdb246af222819050a04ecec
cwen96 <chriswen96@gmail.com>
2021-01-10 00:42:47 -0800

    finished implementing speech to text w/ google api

--- Text_Processing.py
+++ Text_Processing.py
@@ -32,4 +32,3 @@ class Text_Processing(object):
         return sentences
 
     
-
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import tkinter
 from tkinter.filedialog import askopenfile
 import Text_Processing
 import Speech_Processing
+from google.cloud import speech
+import io
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\Chris\\Desktop\\GitHub\\nwHacks2021\\gold-rope-301305-872a7baadfbc.json'
 
 def readFile():
     file = askopenfile(mode = 'rb', filetypes = [("Text Files", "*.txt")])
@@ -11,11 +15,19 @@ def readFile():
         fileContents += str(i, 'utf-8')
     return fileContents
 
+def readAudio():
+    file = askopenfile(mode = 'rb', filetypes = [("all video format", ".wav")])
+    fileContents = file.read()
+    audio = speech.RecognitionAudio(content=fileContents)
+    return audio
+
 def main():
-  contents = readFile()
-  textProcessing = Text_Processing.Text_Processing(contents)
-  topics = textProcessing.getTopics()
-  print(topics)
+  #contents = readFile()
+  #textProcessing = Text_Processing.Text_Processing(contents)
+  audioContents = readAudio()
+  audioProcessing = Speech_Processing.Speech_Processing(audioContents)
+  audioProcessing.SpeechToText()
+
 
 
 
