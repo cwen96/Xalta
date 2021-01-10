6d26a85d69fdcf3b549b8659a77b4f69eb71d107
rohan <rohjag18@gmail.com>
2021-01-09 20:46:21 -0800

    Fixed Space Issue

--- Text_Processing.py
+++ Text_Processing.py
@@ -9,12 +9,19 @@ class Text_Processing(object):
     def __init__(self, text):
         self.text = text
         self.nlp = spacy.load("en_core_web_sm")
+
+    def whitespaceRemover(self, string):
+        text2 = string.replace("\n","")
+        text3 = text2.replace("\r","")
+        return text3
     
     # Returns a list of 5 of the most frequent words that are not stop words and not punctuation
     def getTopics(self):
-        doc = self.nlp(self.text)
+        text2 = self.whitespaceRemover(self.text)
+        doc = self.nlp(text2)
         potentialTopics = [token.text for token in doc if not token.is_stop and not token.is_punct]
         freq = Counter(potentialTopics)
+        del freq[" "]
         listOfTopics = freq.most_common(5)
         return listOfTopics
     
@@ -24,5 +31,5 @@ class Text_Processing(object):
         sentences = list(doc.sents)
         return sentences
 
-
+    
 
